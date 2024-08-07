8331d4de88d1772aa21b01923b977c65c4129640
Mac <chanminswe26@gmail.com>
2024-08-07 22:07:04 +0630

    Added user token

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,65 +2,68 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const loginUser = async (req , res) => {
-    const {email , password } = req.body; 
-    if(!email || !password){
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
         res.status(400);
         throw new Error('All field are mandatory!')
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     //compare password and hashpasswords
-    if(user && (await bcrypt.compare(password , user.password))){
+    if (user && (await bcrypt.compare(password, user.password))) {
         const accesstoken = jwt.sign({
-            username : user.username ,
-            email : user.email,
-            id : user.id
-        })
-        res.status(200).json({accesstoken});
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn : "1m"} )
+        res.status(200).json({ accesstoken });
     }
-
-
-    res.json({message : 'login User'})
-
+    else{
+        res.status(401)
+        throw new Error('email or password is not valid')
+    }
 }
 
-const currentUser =  async (req , res) => {
-    res.json({message : 'Current User Information'})
+const currentUser = async (req, res) => {
+    res.json({ message: 'Current User Information' })
 }
 
-const registerUser = async (req , res) => {
-    const {username , email , password } = req.body;
-    if(!username || !email || !password ){
+const registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
         res.status(400);
         throw new Error('All fields are mandatory')
     }
 
-    const userAvailable = await User.findOne({email});
+    const userAvailable = await User.findOne({ email });
 
-    if(userAvailable){
+    if (userAvailable) {
         res.status(400);
-        throw new Error ('email address already exist')
+        throw new Error('email address already exist')
     }
 
-    const hashedPassword = await bcrypt.hash(password , 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     console.log("Hashed Password is " + hashedPassword)
     const user = await User.create({
-        username ,
-        email , 
+        username,
+        email,
         password: hashedPassword,
     });
 
     console.log(`User created ${user}`);
-    if (user){
-        res.status(201).json({_id : user.id , email : user.email} )
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email })
     }
-    else{
+    else {
         res.status(400)
         throw new Error('User Data not valid!')
     }
 }
 
 
-module.exports = {loginUser , currentUser , registerUser}
+module.exports = { loginUser, currentUser, registerUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const {registerUser , currentUser , loginUser} = require('../controllers/userController')
+const { registerUser, currentUser, loginUser } = require('../controllers/userController')
 
 const router = express.Router()
 
-router.post('/register' ,registerUser )
+router.post('/register', registerUser)
 
-router.post('/login' , currentUser)
+router.post('/login', loginUser);
 
-router.get('/current' , loginUser )
+router.get('/current', currentUser)
 
 module.exports = router
